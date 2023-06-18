63b40167c41f6629c1769e2aed43904d4e3102bf
sonyarianto <sony@sony-ak.com>
2023-06-18 14:50:27 +0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+  import { configureStore, createSlice } from '@reduxjs/toolkit';
+  import { onMount, afterUpdate } from 'svelte';
+
+  const counterSlice = createSlice({
+    name: 'counter',
+    initialState: { value: 0 },
+    reducers: {
+      increment: state => {
+        state.value += 1;
+      },
+      decrement: state => {
+        state.value -= 1;
+      },
+    },
+  });
+
+  const store = configureStore({
+    reducer: {
+      counter: counterSlice.reducer,
+    },
+  });
+
+  let count = 0;
+  let unsubscribe;
+
+  onMount(() => {
+    // Subscribe to changes in the Redux store
+    
+    unsubscribe = store.subscribe(() => {
+      count = store.getState().counter.value;
+    });
+
+    return unsubscribe; // Unsubscribe from the store on component cleanup
+  });
+
+  // Trigger reactivity within the component
+  afterUpdate(() => {
+    count;
+  });
+</script>
+
+<div>
+  <h1>Counter: {count}</h1>
+  <button on:click={() => store.dispatch(counterSlice.actions.increment())}>Increment</button>
+  <button on:click={() => store.dispatch(counterSlice.actions.decrement())}>Decrement</button>
+</div>
